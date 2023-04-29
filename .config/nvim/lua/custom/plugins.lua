c7c496e9398fbcaff0d0a7f2abc6456bd121fc0a
dankuri <danek.il420@gmail.com>
2023-04-29 12:58:01 +0200

    configure rust tools & crates

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -72,6 +72,24 @@ local plugins = {
   },
   {
     "simrat39/rust-tools.nvim",
+    ft = "rust",
+  },
+  {
+    "saecki/crates.nvim",
+    ft = { "rust", "toml" },
+    config = function(_, opts)
+      local crates = require "crates"
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require "plugins.configs.cmp"
+      table.insert(M.sources, 3, { name = "crates" })
+      return M
+    end,
   },
   {
     "kylechui/nvim-surround",
