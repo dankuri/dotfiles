af2c7215cd869efafb8c62fd8795de3c4cf3135e
dankuri <danek.il420@gmail.com>
2023-04-29 12:55:59 +0200

    use tmux navigation

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -4,6 +4,10 @@ M.general = {
   n = {
     ["<C-d>"] = { "<C-d>zz", "page_down & center" },
     ["<C-u>"] = { "<C-u>zz", "page_down & center" },
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "tmux window left" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "tmux window right" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "tmux window down" },
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "tmux window up" },
   },
 }
 return M
