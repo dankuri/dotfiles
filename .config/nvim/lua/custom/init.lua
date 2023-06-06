59dc0b0756f0c2c0895066ac2411f1557cce8b32
dankuri <danek.il420@gmail.com>
2023-06-06 22:07:34 +0200

    use scrolloff & disable tmux nav wrapping

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -1,5 +1,6 @@
 vim.opt.relativenumber = true
 vim.opt.wrap = false
+vim.opt.scrolloff = 8
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -7,6 +8,8 @@ vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.relativenumber = true
 vim.g.autoformat_enabled = true
+vim.g.tmux_navigator_disable_when_zoomed = 1
+vim.g.tmux_navigator_no_wrap = 1
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("highlight_yank", {}),
   desc = "Hightlight selection on yank",
