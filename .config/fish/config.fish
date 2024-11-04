fa4df3dd25688c03197733cef24025e797cdaf72
dankuri <danek.il420@gmail.com>
2024-11-04 21:55:58 +0100

    add nextd & prevd keybinds + move fzf integration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,11 +7,15 @@ set -x ERL_AFLAGS "-kernel shell_history enabled"
 set -U fish_greeting
 
 zoxide init fish | source
-fzf --fish | source
 mise activate fish | source
 
 if status is-interactive
     fish_hybrid_key_bindings # vim editing + emacs keybindings
+    fzf --fish | source
+
+    bind -M insert \el 'nextd && commandline -f repaint'
+    bind -M insert \eh 'prevd && commandline -f repaint'
+
     alias cr "clear"
     alias e "exit"
     alias ls "eza"
